e44aedf9ba57e21c81a875aac91d0b8844a4fdcb
NamBui27 <ares.victorial.98@gmail.com>
2020-03-09 17:33:43 +0700

    update form shopping cart

--- src/app/cart-details/cart-details.component.ts
+++ src/app/cart-details/cart-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart-service/cart.service';
 
@@ -12,11 +12,29 @@ import { CartService } from '../cart-service/cart.service';
 
 export class CartDetailComponent implements OnInit{
     items;
+    checkoutForm;
 
     constructor(
-        private cartService: CartService
-    ){}
+        private cartService: CartService,
+        private formBuilder: FormBuilder
+    ){
+        this.checkoutForm = this.formBuilder.group({
+            name: '',
+            address: '',
+            phone: ''
+        });
+    }
+
     ngOnInit(){
         this.items = this.cartService.getItems();
+    }
+
+    onSubmit(customerData){
+        //process checkout data
+        this.items = this.cartService.clearCart();
+        this.checkoutForm.reset();
+
+        console.warn('your order has been submitted', customerData);
+
     }
 }
